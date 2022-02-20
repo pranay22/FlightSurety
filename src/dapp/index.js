b396cab774879847de5ea015c04dca10d100bcc8
Pranay Sarkar <pranaysarkar22@gmail.com>
2022-02-20 23:46:29 +0100

    na: done [Pranay Sarkar] [V]

--- src/dapp/index.js
+++ src/dapp/index.js
@@ -29,10 +29,69 @@ import './flightsurety.css';
            const key = await contract.fetchFlightStatus(airline, flightCode, departureTime);
            console.log(key) //todo
         })
-    
-    });
-    
+        DOM.elid('btn-register-airline').addEventListener('click', async () => {
+            const newAirline = DOM.elid('input-reg-airline-addr').value;
+            console.log(newAirline);
+            let fee = DOM.elid('input-fund').value;
+            console.log(fee);
+            const { address, votes, contractFee, error } = await contract.fundAndRegisterAirline(fee,newAirline)
+            console.log(address);
+            console.log(votes);
+            console.log(error);
+            display(
+              `Airline ${address}`,
+              'Register Airline', [{
+                label: newAirline,
+                error: error,
+                value: `The fee is ${contractFee} and ${votes} more vote(s) required`
+              }]
+            )
+        })
+        DOM.elid('btn-register-flight').addEventListener('click', async () => {
+        const flightCode = DOM.elid('input-flight-flightCode').value
+        const origin = DOM.elid('input-flight-origin').value            
+        const destination = DOM.elid('input-flight-destination').value
+        const departureTime = new Date(DOM.elid('input-flight-departureTime').value).getTime()
+        const ticketFee = DOM.elid('input-flight-ticketFee').value
+        await contract.registerFlight(
+            flightCode,
+            origin,
+            destination,
+            departureTime,
+            ticketFee
+            )    
+        })
+        DOM.elid('btn-buy-flight').addEventListener('click', async () => {
+            const airlineAddress = DOM.elid('input-buy-flight-airline-address').value
+            const flightCode = DOM.elid('input-buy-flight-flightCode').value
+            const departureTime = new Date(DOM.elid('input-buy-flight-departureTime').value).getTime()
+            const customerAddress = DOM.elid('input-buy-flight-customer-address').value
+            const payment = DOM.elid('input-buy-flight-payment').value
 
+            const { customer, error } = await contract.getFlightTicketAndBuyInsurance(
+                airlineAddress,
+                flightCode,
+                departureTime,
+                customerAddress,
+                payment)
+                display(
+                  `Customer ${customer}`,
+                  'Bought flight and insurance',
+                  [{
+                    label: `Flight with code ${flightCode} departures at ${departureTime}`,
+                    error: error,
+                    value: `payment: ${payment} ETH`
+                  }]
+                )
+        })
+        DOM.elid('btn-pay-insurance').addEventListener('click', () => {
+            try {
+              contract.payInsuranceToCustomer()
+            } catch (error) {
+              console.log(error.message)
+            }
+        })
+    });
 })();
 
 
